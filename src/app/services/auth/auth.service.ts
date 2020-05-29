e0f34a620ea1a15a4eb64e55f894a70bd77c3a61
Ramon Figuera <refm.130995@gmail.com>
2020-05-29 15:56:35 -0400

    Servicio para Usuarios comentado

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api/api.service';
+import { UserData } from 'src/app/interfaces/user-data';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,46 @@ import { ApiService } from '../api/api.service';
 export class AuthService {
 
   constructor(
-    private _Http: HttpClient,
     private api: ApiService
   ) { }
 
-  public registerUser(data) {
+
+    /**
+   * Método para registrar un usuario
+   * @param data Datos del usuario*
+   * **/
+  public signUp(data: UserData) {
+    return new Promise((resolve, reject) => {
+      const seq = this.api.get('api/v1/clientes/listarTodos');
+      seq.subscribe((res: any) => {
+        resolve(res);
+        console.log(res);
+      }, err => {
+        reject(err);
+      });
+    })
+  }
+
+    /**
+   * Método para iniciar sesion un usuario
+   * @param data Datos del usuario*
+   * **/
+  public signIn(data: UserData) {
+    return new Promise((resolve, reject) => {
+      const seq = this.api.get('api/v1/clientes/listarTodos');
+      seq.subscribe((res: any) => {
+        resolve(res);
+        console.log(res);
+      }, err => {
+        reject(err);
+      });
+    })
+  }
+
+     /**
+   * Método para obtener la informacion de un usuario
+   * **/
+  public getUser() {
     return new Promise((resolve, reject) => {
       const seq = this.api.get('api/v1/clientes/listarTodos');
       seq.subscribe((res: any) => {
