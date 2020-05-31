8b631f8a3fb0471cf5586c69f38c3ecf8497b8f9
bryan <bryanjose84@hotmail.com>
2020-05-31 14:05:05 -0400

    pantallas anuncio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,11 @@ const routes: Routes = [
   {
     path: 'explore',
     loadChildren: () => import('./pages/explore/explore.module').then( m => m.ExplorePageModule)
+  },  {
+    path: 'advertisements',
+    loadChildren: () => import('./pages/advertisements/advertisements.module').then( m => m.AdvertisementsPageModule)
   }
+
 
 ];
 @NgModule({
