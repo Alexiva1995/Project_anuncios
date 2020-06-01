3a32c491b3c87e0728678b495c1faa5abd3ebd63
Ramon Figuera <refm.130995@gmail.com>
2020-06-01 14:57:08 -0400

    subscription onesignal

--- src/app/notifications.service.ts
+++ src/app/notifications.service.ts
@@ -12,6 +12,7 @@ export class NotificationsService {
     this.oneSignal.startInit('c1f688f2-217d-4a8f-b019-127cbfc8014c', '245562045849');
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.setSubscription(true);
     this.oneSignal.sendTag("token", localStorage.getItem(CONSTANTES.LOCAL_STORAGE.token));//token
     this.oneSignal.handleNotificationOpened()
     .subscribe(jsonData => {
