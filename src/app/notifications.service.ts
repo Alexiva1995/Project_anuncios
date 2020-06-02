fc9a369704346e2f4c14e4a6c7f0882306793d90
Ramon Figuera <refm.130995@gmail.com>
2020-06-01 21:09:09 -0400

    fcm

--- src/app/notifications.service.ts
+++ src/app/notifications.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { FCM } from '@ionic-native/fcm/ngx'
 import { CONSTANTES } from './services/constantes';
 @Injectable({
   providedIn: 'root'
@@ -7,32 +7,27 @@ import { CONSTANTES } from './services/constantes';
 export class NotificationsService {
   userId: string;
 
-  constructor(private oneSignal: OneSignal) { }
+  constructor(private fcm: FCM) { }
 
+
+  
   public handlerNotifications(){
-    this.oneSignal.startInit('c1f688f2-217d-4a8f-b019-127cbfc8014c', '245562045849');
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
-    this.oneSignal.setSubscription(true);
-    this.oneSignal.sendTag("token", localStorage.getItem(CONSTANTES.LOCAL_STORAGE.token));//token
-    this.oneSignal.handleNotificationOpened()
-    .subscribe(jsonData => {
-  /*     let alert = this.alertCtrl.create({
-        title: jsonData.notification.payload.title,
-        subTitle: jsonData.notification.payload.body,
-        buttons: ['OK']
-      });
-      alert.present();
-      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData)); */
-    });
-    this.oneSignal.endInit();
-    this.oneSignal.getIds().then(onesignal=>{
-      this.userId = onesignal.userId
-    }).catch(error=>{
-      console.log(error)
-    })
+    this.fcm.getToken().then(token => {
+      //backend.registerToken(token
     
-    this.oneSignal.endInit();
+    });
+    
+    this.fcm.onNotification().subscribe(data => {
+      if(data.wasTapped){
+        console.log("Received in background");
+      } else {
+        console.log("Received in foreground");
+      };
+    });
+    
+    this.fcm.onTokenRefresh().subscribe(token => {
+      //backend.registerToken(token);
+    });
   }
 
   private disabled(){
