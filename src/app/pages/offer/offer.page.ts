8d3021c8aed2ffb35ab2cb73724dd0b6850ac773
bryan <bryanjose84@hotmail.com>
2020-06-02 13:47:29 -0400

    test

--- src/app/pages/offer/offer.page.ts
+++ src/app/pages/offer/offer.page.ts
@@ -26,4 +26,8 @@ export class OfferPage implements OnInit {
     this.ruta.navigateForward([`/uploadphoto/${JSON.stringify(this.formGroup.value)}`])
   }
 
+  salir(){
+    this.ruta.back()
+  }
+
 }
